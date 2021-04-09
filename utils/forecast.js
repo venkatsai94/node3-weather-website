49f2b48917682591f3eeee33c27c83abb5a70bfa
venkatsai darshetty <venkatsaidarshetty94@gmail.com>
2021-04-09 17:43:07 +0530

    add high and low to forecast

--- utils/forecast.js
+++ utils/forecast.js
@@ -10,7 +10,7 @@ callback('unable to connect to weather service!',undefined)
 } else if (body.error) {
 callback('unable to find location',undefined)
 } else {
-callback(undefined,body.current.weather_descriptions[0] +' . it is currently '+ body.current.temperature+'degrees out.it feels like'+ body.current.feelslike + ' degreesout.')
+callback(undefined,body.current.weather_descriptions[0]+ " . it is currently "+ body.current.temperature +"degrees out.it feels like "+ body.current.feelslike + " degrees out.The humidity is " + body.current.humidity + "%.")
 }
 
 })
